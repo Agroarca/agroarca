b333e5f7be26de2cef12c7eb9266ed3786990586
Lucas Tomaz <lucas.tomaz@w2z.com.br>
2022-02-01 22:15:20 -0300

    [header/bootstrap] refatorando classes para bootstrap5

--- resources/views/components/site/menu.blade.php
+++ resources/views/components/site/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="container menu">
+<div class="menu">
     <div></div>
     <a class="menu-item" href="{{ route('site.categoria', null) }}">Todas as Categorias</a>
     <a class="menu-item" href="{{ route('site.produto', 1) }}">Produto Exemplo</a>
--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,13 +1,20 @@
 <html>
-    <head>
-        <link href="{{ mix('css/vendor.css') }}" rel="stylesheet">
-        <link href="{{ mix('css/site.css') }}" rel="stylesheet">
-        {{-- <script defer src="{{ mix('js/site.js') }}"></script> --}}
-    </head>
-    <body>
-        <div class="reference"></div>
-        <x-site.header></x-site.header>
 
-        {{ $slot }}
-    </body>
+<head>
+    <link href="{{ mix('css/vendor.css') }}" rel="stylesheet">
+    <link href="{{ mix('css/site.css') }}" rel="stylesheet">
+    <link href="{{ asset('bootstrap5/css/bootstrap.css') }}" rel="stylesheet">
+    {{-- <script defer src="{{ mix('js/site.js') }}"></script> --}}
+</head>
+
+<body>
+    <div class="reference"></div>
+    <x-site.header></x-site.header>
+
+    {{ $slot }}
+
+    <script src="{{ asset('bootstrap5/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/jquery3.6.0.js') }}"></script>
+</body>
+
 </html>
