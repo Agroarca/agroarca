3c04622b49806b593e323019c282c053612f6de6
Eduardo Pilati <pilatieduardo@gmail.com>
2022-02-05 14:42:03 -0300

    compilando assets

---
+++ public/js/admin.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./resources/js/admin.js ***!
+  \*******************************/
+$(function () {
+  $('[data-toggle="tooltip"]').each(function () {
+    $(this).tooltip({
+      container: this
+    });
+  });
+});
+/******/ })()
+;
