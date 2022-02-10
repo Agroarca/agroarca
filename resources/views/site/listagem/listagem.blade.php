e05f6d78f7188940c01a6ca0fd3cd10ab9fe9f0d
Lucas Tomaz <lucas.tomaz@w2z.com.br>
2022-02-09 21:21:17 -0300

    [listagem/categoria] adicionando secao de filtros/ordenacao

--- resources/views/site/listagem/listagem.blade.php
+++ resources/views/site/listagem/listagem.blade.php
@@ -11,8 +11,30 @@
     </section>
 
     <section class="container listagem">
-        {{-- @TODO: Fix vitrine produto fora de grid --}}
+
+        <div class="section-meta">
         <h2 class="section-title">Produtos</h2>
+        <div class="section-filters">
+                <div class="grid-selectable">
+                    <i class="fa fa-th active" aria-hidden="true"></i>
+                    <i class="fas fa-list"></i>
+                </div>
+                <div class="dropdown">
+
+                <span>Ordenar por</span>
+                    <button class="btn btn-secondary dropdown-toggle"type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                      Popularidade
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <li><a class="dropdown-item" href="#">Popularidade</a></li>
+                        <li><a class="dropdown-item" href="#">Menor preço</a></li>
+                    </ul>
+                  </div>
+            </div>
+        </div>
+
+        {{-- @TODO: Fix vitrine produto fora de grid --}}
+        {{-- PROBLEMA: width 90%, porem padding ou margin quebra o grid. --}}
         <div class="produtos">
             @foreach ($produtos as $produto)
                 @include('site.listagem.produto', $produto)
