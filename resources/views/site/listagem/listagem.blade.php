9c3bd413482a38342759ff90725aea479b63532b
Lucas Tomaz <lucas.tomaz@w2z.com.br>
2022-02-09 22:14:33 -0300

    [categoria/listagem] adicionando 'recently viewed' + owl carousel funcionando

--- resources/views/site/listagem/listagem.blade.php
+++ resources/views/site/listagem/listagem.blade.php
@@ -41,4 +41,32 @@
             @endforeach
         </div>
     </section>
+
+    <section class="container">
+        <div class="section-meta">
+            <h2 class="section-title">Produtos Visualizados Recentemente</h2>
+            <div class="section-nav">
+                <i class="fa fa-chevron-left" aria-hidden="true" id="recently-viewed-nav-left"></i>
+                <i class="fa fa-chevron-right" aria-hidden="true" id="recently-viewed-nav-right"></i>
+            </div>
+        </div>
+        <div class="recently-viewed listagem owl-carousel owl-theme" id="recently-viewed-carousel">
+
+            {{-- @TODO: Componentizar para utilizar com include. --}}
+            @foreach(range(1, 10) as $i)  {{-- Loop apenas para testes. --}}
+            <div class="product-showcase-item">
+                <div class="showcase-item-image"
+                {{-- @TODO: Quando retornar do backend, utilizar como background-image. style="background-image()" --}}
+                ></div>
+
+                <div class="product-details">
+                    <h4 class="showcase-title">Titulo do produto</h4>
+                    <span class="preco-produto">R$ 512,00</span>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </section>
+
+    <section></section>
 </x-site>
