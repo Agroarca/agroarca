0a3702674ed9a413aa58b743177a3ed38d424331
Eduardo Pilati <pilatieduardo@gmail.com>
2022-02-12 16:25:30 -0300

    Otimização List Service

--- resources/views/site/listagem/produto.blade.php
+++ resources/views/site/listagem/produto.blade.php
@@ -10,6 +10,6 @@
 <a class="produto" id="produto-{{ $produto->id }}" href="{{ route('site.produto', $produto->id) }}">
     <img class="imagem-produto" src="{{ asset("storage/produtos/$imagem->nome_arquivo") }}" alt={{ $imagem->descricao }}>
     <span class="titulo-produto">{{ $produto->nome }}</span>
-    <span class="preco-produto">{{ Formatter::preco($produto->itensListaPreco->first()->calculaPreco()) }} Kg</span>
+    <span class="preco-produto">{{ Formatter::preco($produto->itensListaPreco->first()->preco_item) }} Kg</span>
     <button class="btn-produto">Quero saber mais!</button>
 </a>
