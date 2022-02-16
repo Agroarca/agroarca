612d646ec6f13224d066323a5e0e36e05923b92d
Eduardo Pilati <pilatieduardo@gmail.com>
2022-02-15 21:07:30 -0300

    Compilando assets

--- public/js/script.js
+++ public/js/script.js
@@ -15566,7 +15566,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var owl = $('#recently-viewed-carousel').owlCarousel({
+var owlRecentlyViewed = $("#recently-viewed-carousel").owlCarousel({
   margin: 10,
   loop: true,
   nav: false,
@@ -15582,11 +15582,33 @@ var owl = $('#recently-viewed-carousel').owlCarousel({
     }
   }
 });
-$('#recently-viewed-nav-right').click(function () {
-  owl.trigger('next.owl.carousel');
+$("#recently-viewed-nav-right").click(function () {
+  owlRecentlyViewed.trigger("next.owl.carousel");
 });
-$('#recently-viewed-nav-left').click(function () {
-  owl.trigger('prev.owl.carousel', [300]);
+$("#recently-viewed-nav-left").click(function () {
+  owlRecentlyViewed.trigger("prev.owl.carousel", [300]);
+});
+var owlFlashSale = $("#flash-sale-carousel").owlCarousel({
+  margin: 10,
+  loop: true,
+  nav: false,
+  responsive: {
+    0: {
+      items: 2
+    },
+    600: {
+      items: 2
+    },
+    1000: {
+      items: 2
+    }
+  }
+});
+$("#flashsale-nav-right").click(function () {
+  owlFlashSale.trigger("next.owl.carousel");
+});
+$("#flashsale-nav-left").click(function () {
+  owlFlashSale.trigger("prev.owl.carousel", [300]);
 });
 })();
 
