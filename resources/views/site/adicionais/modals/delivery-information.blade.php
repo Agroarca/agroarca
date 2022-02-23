709f142f2ec69ab182e3feb930c1674724caa45c
Lucas Tomaz <lucas.tomaz@w2z.com.br>
2022-02-23 17:05:24 -0300

    [delivery info] movendo para modal componentizado + estilizado

--- resources/views/site/adicionais/modals/delivery-information.blade.php
+++ resources/views/site/adicionais/modals/delivery-information.blade.php
@@ -7,41 +7,24 @@
     }
 </style>
 
-{{-- @TODO: Falta estilizar modal conforme layout modelo figma. --}}
-
-{{--
-    Pensar em alguma forma de componentizar o modal, para um layout base.
-
-    https://www.figma.com/file/OIUAXTc9iZXZLAwndWiLFs/AgroArca---Pilati---Mobile-KIT-UI---Entrega?node-id=0%3A1
-    --}}
-<div class="modal" tabindex="-1" id="delivery-info-modal">
-    <div class="modal-dialog  modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Como deseja sua entrega?</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<x-site.modal :title="'Como deseja sua entrega?'" :id="'delivery-info-modal'">
+    <form onsubmit="preventDefault();" id="form-delivery-info">
+        <div class="row">
+            <div class="input-group mb-3">
+                {{-- @TODO: Implementar inputmask --}}
+                <span class="input-group-text" id="inputGroupCep">CEP</span>
+                <input type="text" class="form-control" id="cep" name="cep" aria-describedby="inputGroupCep" required>
+            </div>
+            <div class="input-group">
+                <span class="input-group-text" id="inputGroupDate">Data de Entrega</span>
+                <input type="date" class="form-control" id="delivery-date" name="deliveryDate" aria-describedby="inputGroupDate" required>
+            </div>
         </div>
-        <div class="modal-body">
+    </form>
 
-            <form onsubmit="preventDefault();" id="form-delivery-info">
-                <div class="row">
-                    <div class="input-group mb-3">
-                        {{-- @TODO: Implementar inputmask --}}
-                      <span class="input-group-text" id="inputGroupCep">CEP</span>
-                      <input type="text" class="form-control" id="cep" name="cep" aria-describedby="inputGroupCep" required>
-                    </div>
-                    <div class="input-group">
-                      <span class="input-group-text" id="inputGroupDate">Data de Entrega</span>
-                      <input type="date" class="form-control" id="delivery-date" name="deliveryDate" aria-describedby="inputGroupDate" required>
-                    </div>
-                </div>
-            </form>
+    <x-slot name="footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+        <button type="button" id="submit-delivery-form" class="btn btn-primary">Salvar</button>
+    </x-slot>
+</x-site.modal>
 
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-          <button type="button" id="submit-delivery-form" class="btn btn-primary">Salvar</button>
-        </div>
-      </div>
-    </div>
-  </div>
