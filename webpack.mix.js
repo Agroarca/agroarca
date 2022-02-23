4c31becb490e800da74dae38f76e17cb68050eb3
Lucas Tomaz <lucas.tomaz@w2z.com.br>
2022-02-23 20:57:32 -0300

    [scss/webpack] derivando custom.scss do vendor.scss para estilos em componentes

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,7 +18,7 @@
  */
 
 const mix = require("laravel-mix");
-require('laravel-mix-merge-manifest')
+require("laravel-mix-merge-manifest");
 mix.disableNotifications();
 
 mix.webpackConfig({
@@ -32,26 +32,25 @@ mix.webpackConfig({
  |--------------------------------------------------------------------------
 */
 
-if(process.env.WITHOUT && process.env.WITHOUT == "vendor"){
-
+if (process.env.WITHOUT && process.env.WITHOUT == "vendor") {
     mix.postCss("resources/css/style.css", "public/css/")
-       .postCss("resources/css/admin.css", "public/css/")
-       .js("resources/js/script.js", "public/js")
-       .autoload({ jquery: ['$', 'window.jQuery', 'jQuery'] })
-       .js("resources/js/admin.js", "public/js")
-       .mergeManifest();
-
-}else{
-
+        .postCss("resources/css/admin.css", "public/css/")
+        .js("resources/js/script.js", "public/js")
+        .autoload({ jquery: ["$", "window.jQuery", "jQuery"] })
+        .js("resources/js/admin.js", "public/js")
+        .sass("resources/sass/custom.scss", "public/css/style.css")
+        .mergeManifest();
+} else {
     mix.postCss("resources/css/vendor.css", "public/css/")
-       .sass("resources/sass/vendor.scss", "public/css/vendor.css")
-       .postCss("resources/css/vendor-admin.css", "public/css/")
-       .js("resources/js/vendor.js", "public/js")
-       .js("resources/js/vendor-admin.js", "public/js")
+        .sass("resources/sass/vendor.scss", "public/css/vendor.css")
+        .postCss("resources/css/vendor-admin.css", "public/css/")
+        .js("resources/js/vendor.js", "public/js")
+        .js("resources/js/vendor-admin.js", "public/js")
 
-       .postCss("resources/css/style.css", "public/css/")
-       .postCss("resources/css/admin.css", "public/css/")
-       .js("resources/js/script.js", "public/js")
-       .autoload({ jquery: ['$', 'window.jQuery', 'jQuery'] })
-       .js("resources/js/admin.js", "public/js")
+        .postCss("resources/css/style.css", "public/css/")
+        .postCss("resources/css/admin.css", "public/css/")
+        .js("resources/js/script.js", "public/js")
+        .autoload({ jquery: ["$", "window.jQuery", "jQuery"] })
+        .js("resources/js/admin.js", "public/js")
+        .sass("resources/sass/custom.scss", "public/css/style.css");
 }
