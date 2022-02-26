373ae1f5b913ac92b3c6452c6361ac5e6ac897d3
eduardpilati <pilatieduardo@gmail.com>
2022-02-26 10:37:33 -0300

    Limpeza do projeto
    Removendo arquivos que não são usados, bilbiotecas antigas ...

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -1,11 +1,8 @@
 <html>
 
 <head>
-    {{-- <link href="{{ asset('bootstrap5/css/bootstrap.css') }}" rel="stylesheet"> --}}
     <link href="{{ mix('css/vendor.css') }}" rel="stylesheet">
     <link href="{{ mix('css/style.css') }}" rel="stylesheet">
-
-
 </head>
 
 <body>
@@ -15,8 +12,8 @@
     {{ $slot }}
 
     <x-site.footer></x-site.footer>
-    {{-- <script src="{{ asset('bootstrap5/js/bootstrap.js') }}"></script> --}}
     <script src="{{ mix('js/script.js') }}"></script>
     <script src="{{ mix('js/vendor.js') }}"></script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
-require __DIR__.'/admin.php';
-require __DIR__.'/site.php';
+require __DIR__ . '/auth.php';
+require __DIR__ . '/admin.php';
+require __DIR__ . '/site.php';
