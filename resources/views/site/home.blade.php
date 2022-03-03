bfdf5f820c2751305a3e97626fe4c95e57316504
Lucas Tomaz <lucas.tomaz@w2z.com.br>
2022-03-02 21:37:28 -0300

    [banner section] adicionando em listagem/home + imagens placeholder

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -1,6 +1,24 @@
 <x-site>
 <p class="container">Home page xD</p>
 
+@php
+    $items = [];
+    foreach(range(1, 10) as $i){
+        $items[] = [
+            'subtitle' => 'Produtos de Qualidade',
+            'title' => 'Sementes, fertilizantes e produtos para plantio.',
+            'image' => asset('img/temp/example'.($i % 2 ? 1 : 2).'.png'),
+            'action' => 'http://google.com',
+            'actionTitle' => 'Ver Produtos'
+        ];
+    }
+@endphp
+
+@include('site.adicionais.banner-section', [
+    'multiple' => true,
+    'data' => $items
+])
+
 @include('site.adicionais.business-values')
 
 @if(isset($flash_sale) && $flash_sale)
