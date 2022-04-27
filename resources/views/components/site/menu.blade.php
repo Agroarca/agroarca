5981951034e07303753cb2c04539f88269d8ea9b
eduardpilati <pilatieduardo@gmail.com>
2022-04-27 13:30:06 -0300

    teste

--- resources/views/components/site/menu.blade.php
+++ resources/views/components/site/menu.blade.php
@@ -3,6 +3,7 @@
     <a class="menu-item" href="{{ route('site.categoria', null) }}">Todas as Categorias</a>
     @if (!is_null(\App\Models\Estoque\Produto::first()))
         <a class="menu-item" href="{{ route('site.produto', \App\Models\Estoque\Produto::first()) }}">Produto Exemplo</a>
-    @endif
+        @endif
+        <a class="menu-item" href="{{ url('') }}">teste</a>
     <div></div>
 </div>
