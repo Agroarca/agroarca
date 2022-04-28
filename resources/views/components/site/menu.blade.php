ba3b73f5858f41b9f47ad80674e6c3bef05eedd7
eduardpilati <pilatieduardo@gmail.com>
2022-04-28 10:34:06 -0300

    teste

--- resources/views/components/site/menu.blade.php
+++ resources/views/components/site/menu.blade.php
@@ -4,6 +4,5 @@
     @if (!is_null(\App\Models\Estoque\Produto::first()))
         <a class="menu-item" href="{{ route('site.produto', \App\Models\Estoque\Produto::first()) }}">Produto Exemplo</a>
         @endif
-        <a class="menu-item" href="{{ url('') }}">teste</a>
     <div></div>
 </div>
